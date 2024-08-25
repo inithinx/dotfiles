9f8aa54737c0029e3b06240ae8cfd487c84d352a
Nithin Krishnaa <inithin683@gmail.com>
2024-08-25 23:14:15 +0530

    sync: 25-08-24

--- home/eins/default.nix
+++ home/eins/default.nix
@@ -1,24 +1,10 @@
 {pkgs, lib, config, ...}:{
 
 # Home-Manager Setup
-  #programs.fuse.userAllowOther = true; # needed for home manager to persist stuff.
-  #imports = [ impermanence.nixosModules.home-manager.impermanence ];
+  programs.fuse.userAllowOther = true; # needed for home manager to persist stuff.
   home = {
     username = "nithin";
     homeDirectory = "/home/nithin";
     stateVersion = "24.05";
-    #persistence."/persist/home" = {
-    #  directories = [
-    #    "Downloads"
-    #    "Music"
-    #    "Pictures"
-    #    "Documents"
-    #    "Videos"
-    #    { directory = ".ssh"; mode = "0700"; }
-    #  ];
-    #  files = [
-    #    ".bashrc"
-    #  ];
-    #};
   };
 }
